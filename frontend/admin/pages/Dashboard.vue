580979984e0a1b94c6c146df6b806be35396cbda
Andrey Kozhemyakin <kozhemyakin.andrej@gmail.com>
2018-02-09 17:59:44 +0200

    Accordion with slote and alert message component

--- frontend/admin/pages/Dashboard.vue
+++ frontend/admin/pages/Dashboard.vue
@@ -2,12 +2,6 @@
   <section class="dashboard">
     <h1>dashboard Component</h1>
     <k-logout></k-logout>
-    <ui-spinner :show="true"></ui-spinner>
-    <ui-position center relative><p>Hello</p></ui-position>
-    <p>Slider nav section</p>
-    <ui-slider-nav overlay large></ui-slider-nav>
-    <p>End Slider nav section</p>
-    <uk-accordion notCollapsible multiple></uk-accordion>
   </section>
 </template>
 
--- frontend/admin/plugins/uikit/index.js
+++ frontend/admin/plugins/uikit/index.js
@@ -1,11 +1,14 @@
 import "./styles/theme.scss";
+import Accordion from './components/Accordion'
+import AccordionContent from './components/AccordionContent'
+import Alert from './components/Alert'
 import SliderNav from './components/SliderNav'
 import Spinner from './components/Spinner'
 import Position from './components/Position'
 import Container from './components/Container'
 import Card from './components/Card'
 import Icon from './components/Icon'
-import Accordion from './components/Accordion'
+
 import UIkit from 'uikit'
 import Icons from 'uikit-icons'
 
@@ -13,13 +16,15 @@ UIkit.use(Icons)
 
 const uikit = {
     install(Vue, options) {
+        Vue.component(Accordion.name, Accordion);
+        Vue.component(AccordionContent.name, AccordionContent);
+        Vue.component(Alert.name, Alert);
         Vue.component(Card.name, Card);
         Vue.component(Container.name, Container);
         Vue.component(Icon.name, Icon);
         Vue.component(Position.name, Position);
         Vue.component(Spinner.name, Spinner);
         Vue.component(SliderNav.name, SliderNav);
-        Vue.component(Accordion.name, Accordion);
     }
 };
 
