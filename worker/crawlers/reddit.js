78fb3ffd2e5446257dc490c3d5e1ecaab08924e0
Ruslan Komlikov <komlikov.r@gmail.com>
2018-04-10 15:09:12 +0300

    add functions for get new posts from reddit

--- worker/crawlers/reddit.js
+++ worker/crawlers/reddit.js
@@ -2,82 +2,69 @@ const snoowrap = require('snoowrap');
 const axios = require('axios');
 const config  = require('../config');
 
-// TODO "and reddit worker"
-
-
-    let r = new snoowrap({
-        userAgent: 'reddit-crawler-example-app',
-        clientId: 'M1dPE7ODOp61NQ',
-        clientSecret: 'h30ghT9k1G1dzRMKxUEW5AUL_5I',
-        username: 'RumusBin',
-        password: 24031982
-    });
-
-    r.getNew()
-        .then(
-        listing => work(listing)
-    );
-
-    function work(listing){
-
-        console.log(getIssetIds());
-
-            // listing.slice(0).reverse().map(submission => mainWork(submission, 'sum str'));
-        }
-
-    function getIssetIds() {
-        axios.get(config.API_URL + '/api/post/filter?source=reddit&sort=id')
-            .then(
-                response => {
-                    let arrIssetIds = [];
-                    if(response.data.total > 0){
-                        response.data.rows.map(item => arrIssetIds.push(item.content))
-                    }
-                    return arrIssetIds;
-                }
-            )
-            .catch(
-                e => {
-                    console.error(e)
-                }
-            );
-    }
-
-
-     function getIssetIdsArr(object){
-
-     }
-
-    function mainWork(e, str) {
-         console.log(str)
-        // console.log({title: e.title, content: e.id, url: e.url, publishedAt: dateTimeConverter(e.created_utc), source: 'reddit', type: e.link_flair_text});
-            // if(checkNewest(e.id)){
-            //     console.log({title: e.title, content: e.id, url: e.url, publishedAt: dateTimeConverter(e.created_utc), source: 'reddit', type: e.link_flair_text});
-            // }
-    }
-    function checkNewest(redditIds) {
-        let arrIssetIds = getIssetIdsArr();
-        if(redditIds in arrIssetIds ){
-            return false;
-        }return true;
-    }
-
-    function dateTimeConverter(utc_format) {
-        let dt_format = new Date(utc_format * 1000);
-        return dt_format;
-    }
 
 let instance = {
     execute(task, axios) {
         console.log(task.config);
 
 
-        // client.getNew().then(posts => {
-        //
-        //     }
-        // )
+        let r = new snoowrap({
+            userAgent: 'reddit-crawler-example-app',
+            clientId: task.config.clientId,
+            clientSecret: task.config.clientSecret,
+            username: task.config.userName,
+            password: task.config.userPassword
+        });
+
+        r.getNew()
+            .then(
+                getNewRedditArr
+            )
+            .then(
+                mainWork
+            );
+
+        function getNewRedditArr(listing){
+            let reditArr = [];
+            listing.slice(0).reverse().map(e => reditArr.push({title: e.title, content: e.id, url: e.url, publishedAt: dateTimeConverter(e.created_utc), source: 'reddit', type: e.link_flair_text}));
+            return reditArr;
+        }
+
+        function mainWork(newRedditArr) {
+            // get id list from DB
+            axios.get(config.API_URL + '/api/post/filter?source=reddit&sort=id')
+                .then(
+                    response => {
+                        getNewestRedditPosts(response, newRedditArr)
+                    }
+                )
+                .catch(
+                    e => {
+                        console.error(e)
+                    }
+                );
+        }
+
+        function getNewestRedditPosts(response, newRedditArr){
+            let arrIssetIds = [];
+            if(response.data.total > 0){
+                response.data.rows.map(item => arrIssetIds.push(item.content))
+            }
+
+            newRedditArr.map(item =>{
+                // console.log(item.content)
+                if(arrIssetIds.indexOf(item.content) === -1){
+                    console.log(item)
+                }
+            });
+
+        }
+
+        function dateTimeConverter(utc_format) {
+            return new Date(utc_format * 1000);
+        }
 
     }
 };
 
-// module['exports'] = instance;
+module['exports'] = instance;
