f9924992968b29885216cd38765dc841744e61a7
Iakov Mishchenko <iakov@kamilabs.com>
2018-04-10 20:48:23 +0300

    Removed frontend bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -27,7 +27,6 @@ class AppKernel extends Kernel
 
             new Kami\ApiCoreBundle\KamiApiCoreBundle(),
             new Kami\UserBundle\KamiUserBundle(),
-            new Kami\FrontendBundle\KamiFrontendBundle(),
             new Kami\ContentBundle\KamiContentBundle(),
             new Kami\WorkerBundle\KamiWorkerBundle(),
         ];
