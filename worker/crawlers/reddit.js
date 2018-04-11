7aa48eb39095b759bc3e576a1898c344a7b3a7f3
Ruslan Komlikov <komlikov.r@gmail.com>
2018-04-11 15:34:09 +0300

    add anonimous functions

--- worker/crawlers/reddit.js
+++ worker/crawlers/reddit.js
@@ -18,50 +18,36 @@ let instance = {
 
         r.getNew()
             .then(
-                getNewRedditArr
+                response=>{
+                    let newRedditPosts = [];
+                    response.map(e => newRedditPosts.push({title: e.title, url: e.url, content: e.id, source: 'reddit', publishedAt: e.created_utc}));
+                    return newRedditPosts;
+                }
             )
             .then(
-                mainWork
-            );
-
-        function getNewRedditArr(listing){
-            let reditArr = [];
-            listing.map(e => reditArr.push({title: e.title, url: e.url, content: e.id, source: 'reddit', publishedAt: e.created_utc}));
-            return reditArr;
-        }
-
-        function mainWork(lastPosts) {
-            // get id list from DB
-            axios.get(config.API_URL + '/api/post/filter?source=reddit&sort=id')
-                .then(
-                    response => {
-                        getLastPosts(response, lastPosts)
-                    }
-                )
-                .catch(
-                    e => {
-                        console.error(e)
-                    }
-                );
-        }
-
-        function getLastPosts(response, lastPosts){
-
-            let existingPosts =  response.data.rows.map(row => row.content);
-
-            lastPosts.map(post =>{
-                if(existingPosts.indexOf(post.content) === -1){
-                    sendData(post);
+                lastPosts => {
+                    // get id list from DB
+                    axios.get(config.API_URL + '/api/post/filter?source=reddit&sort=id')
+                        .then(
+                            response => {
+                                let existingPosts =  response.data.rows.map(row => row.content);
+                                lastPosts.map(post =>{
+                                    if(existingPosts.indexOf(post.content) === -1){
+                                        axios.post(config.API_URL + '/api/post', {'post': obj})
+                                            .catch(
+                                                e => console.error(e)
+                                            );
+                                    }
+                                });
+                            }
+                        )
+                        .catch(
+                            e => {
+                                console.error(e)
+                            }
+                        );
                 }
-            });
-        }
-
-        function sendData(obj) {
-            axios.post(config.API_URL + '/api/post', {'post': obj})
-                .catch(
-                  e => console.error(e)
-                );
-        }
+            );
     }
 
 };
