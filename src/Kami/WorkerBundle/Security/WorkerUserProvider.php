5feb6c43b4416a23e4238a237d2a33e4af3be52b
rubilnik89 <rubilnik89@gmail.com>
2018-04-12 13:23:16 +0300

    instanceof

--- src/Kami/WorkerBundle/Security/WorkerUserProvider.php
+++ src/Kami/WorkerBundle/Security/WorkerUserProvider.php
@@ -38,6 +38,12 @@ class WorkerUserProvider implements UserProviderInterface
 
     public function refreshUser(UserInterface $user)
     {
+        if (!$user instanceof UserInterface) {
+            throw new UnsupportedUserException(
+                sprintf('Instances of "%s" are not supported.', get_class($user))
+            );
+        }
+
         return $this->loadUserByUsername($user->getUsername());
     }
 
