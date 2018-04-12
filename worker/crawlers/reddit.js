7f48200b678739eb666b5dd7eeadbbfed51866ba
Ruslan Komlikov <komlikov.r@gmail.com>
2018-04-12 16:40:48 +0300

    fix bugs code review

--- worker/crawlers/reddit.js
+++ worker/crawlers/reddit.js
@@ -15,39 +15,37 @@ let instance = {
             username: task.config.userName,
             password: task.config.userPassword
         });
-
         r.getNew()
             .then(
                 response=>{
                     let newRedditPosts = [];
-                    response.map(e => newRedditPosts.push({title: e.title, url: e.url, content: e.id, source: 'reddit', publishedAt: e.created_utc}));
+                    response.slice(0).reverse().map(e => {
+
+                        newRedditPosts.push({title: e.title, url: e.url, content: e.id, source: 'reddit', publishedAt: new Date(e.created_utc * 1000)})
+                    });
+
                     return newRedditPosts;
                 }
-            )
-            .then(
-                lastPosts => {
-                    // get id list from DB
-                    axios.get(config.API_URL + '/api/post/filter?source=reddit&sort=id')
-                        .then(
-                            response => {
-                                let existingPosts =  response.data.rows.map(row => row.content);
-                                lastPosts.map(post =>{
-                                    if(existingPosts.indexOf(post.content) === -1){
-                                        axios.post(config.API_URL + '/api/post', {'post': obj})
-                                            .catch(
-                                                e => console.error(e)
-                                            );
-                                    }
-                                });
+            ).then(
+            newRedditPosts => {
+
+                axios.get(config.API_URL + '/api/post/filter?source=reddit&limit=1&sort=id')
+                    .then(
+                        (response) => {
+                            let lastRedditIdFromDB = '0';
+                            if(response.data.total !== '0'){
+                                lastRedditIdFromDB = response.data.rows[0].content;
                             }
-                        )
-                        .catch(
-                            e => {
-                                console.error(e)
+                            for(let i = 0; i < newRedditPosts.length; i++){
+
+                                if(newRedditPosts.slice(0).reverse()[i].content === lastRedditIdFromDB){
+                                    console.log('breake');
+                                    break
+                                }else  axios.post(config.API_URL + '/api/post', {'post': newRedditPosts[i]})
                             }
-                        );
-                }
-            );
+                        });
+            }
+        );
     }
 
 };
