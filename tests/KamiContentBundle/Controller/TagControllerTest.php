6c4bf0e41b498e775dc11889a556affe03dd7635
rubilnik89 <rubilnik89@gmail.com>
2018-04-13 12:45:20 +0300

    tests

--- tests/KamiContentBundle/Controller/TagControllerTest.php
+++ tests/KamiContentBundle/Controller/TagControllerTest.php
@@ -62,6 +62,111 @@ class TagControllerTest extends ApiTestCase
         $this->assertCount(1, $response['rows']);
     }
 
+    public function testCreateLoggedInAsAnonymous()
+    {
+        $response = $this->request('POST', '/api/tag', ['tag' => ['title' => 'test']]);
+        $this->assertJsonResponse($response, 403);
+    }
+
+    public function testCreateLoggedInAsUser()
+    {
+        $this->logInAsUser();
+        $response = $this->request('POST', '/api/tag', ['tag' => ['title' => 'test']]);
+        $this->assertJsonResponse($response, 403);
+    }
+
+    public function testCreateLoggedInAsAdmin()
+    {
+        $this->logInAsAdmin();
+        $response = $this->request('POST', '/api/tag', ['tag' => ['title' => 'test']]);
+        $this->assertJsonResponse($response, 200);
+    }
+
+    public function testCreateNotExistedFieldLoggedInAsAdmin()
+    {
+        $this->logInAsAdmin();
+        $response = $this->request('POST', '/api/tag', ['tag' => ['name' => 'test']]);
+        $this->assertJsonResponse($response, 400);
+        $this->assertEquals('This form should not contain extra fields.', $this->getResponseData($response)['form']['errors'][0]);
+    }
+
+    public function testSingleLoggedInAsAnonymous()
+    {
+        $response = $this->request('GET', '/api/tag/1');
+        $this->assertJsonResponse($response, 403);
+    }
+
+    public function testSingleLoggedInAsUser()
+    {
+        $this->logInAsUser();
+        $response = $this->request('GET', '/api/tag/1');
+        $this->assertJsonResponse($response, 200);
+    }
+
+    public function testSingleLoggedInAsAdmin()
+    {
+        $this->logInAsAdmin();
+        $response = $this->request('GET', '/api/tag/1');
+        $this->assertJsonResponse($response, 200);
+    }
+
+    public function testSingleNotExistedIdLoggedInAsAdmin()
+    {
+        $this->logInAsAdmin();
+        $response = $this->request('GET', '/api/tag/0');
+        $this->assertJsonResponse($response, 404);
+    }
+
+    public function testEditLoggedInAsAnonymous()
+    {
+        $response = $this->request('PUT', '/api/tag/1', ['tag' => ['title' => 'edit']]);
+        $this->assertJsonResponse($response, 403);
+    }
+
+    public function testEditLoggedInAsUser()
+    {
+        $this->logInAsUser();
+        $response = $this->request('PUT', '/api/tag/1', ['tag' => ['title' => 'edit']]);
+        $this->assertJsonResponse($response, 403);
+    }
+
+    public function testEditLoggedInAsAdmin()
+    {
+        $this->logInAsAdmin();
+        $response = $this->request('PUT', '/api/tag/1', ['tag' => ['title' => 'edit']]);
+        $this->assertJsonResponse($response, 200);
+    }
+
+    public function testEditNotExistedFieldLoggedInAsAdmin()
+    {
+        $this->logInAsAdmin();
+        $response = $this->request('PUT', '/api/tag/1', ['tag' => ['name' => 'edit']]);
+        $this->assertJsonResponse($response, 400);
+        $this->assertEquals('This form should not contain extra fields.', $this->getResponseData($response)['form']['errors'][0]);
+    }
+
+    public function testDeleteLoggedInAsAnonymous()
+    {
+        $response = $this->request('DELETE', '/api/tag/1');
+        $this->assertJsonResponse($response, 403);
+    }
+
+    public function testDeleteLoggedInAsUser()
+    {
+        $this->logInAsUser();
+        $response = $this->request('DELETE', '/api/tag/1');
+        $this->assertJsonResponse($response, 403);
+    }
+
+    public function testDeleteLoggedInAsAdmin()
+    {
+        $this->logInAsAdmin();
+        $response = $this->request('DELETE', '/api/tag/1');
+        $this->assertJsonResponse($response, 201);
+    }
+
+
+
     public function getModelKeys()
     {
         return ['id', 'title'];
