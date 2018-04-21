6d403e7e0ec938a14631eb0f83929c54b9148d09
Iakov Mishchenko <iakov@kamilabs.com>
2018-04-21 13:17:14 +0300

    Deployment to heroku

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -28,13 +28,13 @@ class AppKernel extends Kernel
             new Kami\UserBundle\KamiUserBundle(),
             new Kami\ContentBundle\KamiContentBundle(),
             new Kami\WorkerBundle\KamiWorkerBundle(),
+            new Khepin\YamlFixturesBundle\KhepinYamlFixturesBundle()
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
             $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
-            $bundles[] = new Khepin\YamlFixturesBundle\KhepinYamlFixturesBundle();
 
             if ('dev' === $this->getEnvironment()) {
                 $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
