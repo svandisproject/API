c3c7d45f73f349a3ac8993b8762cea86938b1ce8
Iakov Mishchenko <iakov@kamilabs.com>
2018-04-22 15:34:27 +0300

    Added listener and doctrine migrations

---
+++ src/Kami/WorkerBundle/Listener/PostCreationListener.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace Kami\WorkerBundle\Listener;
+
+
+use Kami\ApiCoreBundle\Event\CrudEvent;
+
+class PostCreationListener
+{
+    public function onKamiApiCoreResourceCreate(CrudEvent $event)
+    {
+
+    }
+}
