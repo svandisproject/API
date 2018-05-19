75f9e29097056b9b1a7d872127d8942691de4c1d
Iakov Mishchenko <iakov@kamilabs.com>
2018-05-19 20:46:35 +0300

    Added IcoBenchBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -27,6 +27,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
             new Lopi\Bundle\PusherBundle\LopiPusherBundle(),
             new Nelmio\ApiDocBundle\NelmioApiDocBundle(),
+            new Kami\IcoBenchBundle\KamiIcoBenchBundle(),
 
             new Kami\ApiCoreBundle\KamiApiCoreBundle(),
             new Kami\UserBundle\KamiUserBundle(),
