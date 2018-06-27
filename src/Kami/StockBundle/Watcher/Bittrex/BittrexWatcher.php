423d9c1b3f5f7070bbccc207a78f64b8ed2bbcda
Ruslan Komlikov <komlikov.r@gmail.com>
2018-06-27 17:45:47 +0300

    add bittrex stock watcher

---
+++ src/Kami/StockBundle/Watcher/Bittrex/BittrexWatcher.php
@@ -0,0 +1,83 @@
+<?php
+
+
+namespace Kami\StockBundle\Watcher\Bittrex;
+
+use Kami\StockBundle\Watcher\AbstractExchangeWatcher;
+use Kami\StockBundle\Watcher\Bittrex\Utils\BittrexClient;
+
+class BittrexWatcher extends AbstractExchangeWatcher
+{
+
+    public function updateAssetPrices()
+    {
+        $client = new BittrexClient();
+        $tickers = $client->getTickers();
+
+        $tickersArray = $this->getUsdPrices($tickers);
+
+        foreach ($tickersArray as $tickerData) {
+            $point = $this->createNewPoint($tickerData);
+
+            $this->persistPoint($point);
+        }
+    }
+
+    public function getUsdPrices(array $tickers)
+    {
+        $points = [];
+        $usdPrices = [];
+
+        foreach ($tickers as $ticker) {
+            foreach ($ticker as $pair => $price) {
+                $currenciesArr = explode('-', $pair);
+                if($currenciesArr[0] == "USD"){
+                    $usdPrices[$pair] = $price;
+                }
+            }
+        }
+        foreach ($tickers as $ticker) {
+            foreach ($ticker as $pair => $price) {
+
+                    $coinsArray = explode('-', $pair);
+                    $currency = $coinsArray[0];
+                    $asset = $coinsArray[1];
+
+                    if ($currency !== "USD") {
+                        $rate = $usdPrices['USD-' . $currency] * $price;
+                        array_push($points, ['asset' => $asset, 'price' => $rate]);
+                    }
+                }
+            }
+
+          $pointsArray = $this->usdPriceNormalize($points);
+
+        return $pointsArray;
+    }
+
+    /**
+     * Get average arithmetic of asset price in USD
+     *
+     * @param array
+     *
+     * @return array
+     */
+    private function usdPriceNormalize($tickersArray) :array
+    {
+        $normalizeArray = [];
+        $resultArray = [];
+        foreach ($tickersArray as $tickers) {
+            if (!key_exists($tickers['asset'], $normalizeArray)) {
+                $normalizeArray[$tickers['asset']] = $tickers['price'];
+            } else {
+                $normalizeArray[$tickers['asset']] = ($normalizeArray[$tickers['asset']] + $tickers['price']) / 2;
+            }
+        }
+        foreach ($normalizeArray as $asset => $price) {
+            array_push($resultArray, ['asset' => $asset, 'price' => $price]);
+        }
+
+        return $resultArray;
+    }
+
+}
