fca1d787b156ac21da2443f50248eeb9dff5229d
rubilnik89 <rubilnik89@gmail.com>
2018-06-27 16:37:15 +0300

    refactor watchers

--- src/Kami/StockBundle/Watcher/Bitfinex/BitfinexWatcher.php
+++ src/Kami/StockBundle/Watcher/Bitfinex/BitfinexWatcher.php
@@ -3,13 +3,8 @@
 
 namespace Kami\StockBundle\Watcher\Bitfinex;
 
-
-use Cassandra\BatchStatement;
-use Cassandra\Uuid;
 use Doctrine\ORM\ORMException;
 use Doctrine\ORM\ORMInvalidArgumentException;
-use Kami\AssetBundle\Entity\Asset;
-use Kami\StockBundle\Model\Point;
 use Kami\StockBundle\Watcher\AbstractExchangeWatcher;
 
 class BitfinexWatcher extends AbstractExchangeWatcher
@@ -59,63 +54,6 @@ class BitfinexWatcher extends AbstractExchangeWatcher
             }
             array_push($this->tickersArray, ['asset' => $presentedCurrency, 'price' => floatval($price)]);
         }
-    }
-
-    /**
-     * @param Point $point
-     * @throws \Cassandra\Exception
-     * @return void
-     */
-    public function persistPoint(Point $point)
-    {
-        $cassandra = $this->client;
-        $prepared = $cassandra->prepare(
-            'INSERT INTO svandis_asset_prices.asset_price (id, ticker, price, time) 
-              VALUES (?, ?, ?, toTimeStamp(toDate(now())));'
-        );
-        $batch = new BatchStatement(\Cassandra::BATCH_LOGGED);
-        $pointDbValues = $point->toDatabaseValues();
-
-        $batch->add($prepared, [
-            'id' => new Uuid(\Ramsey\Uuid\Uuid::uuid1()->toString()),
-            'ticker' => $pointDbValues['asset'],
-            'price' =>  new \Cassandra\Float($pointDbValues['price'])
-        ]);
-
-        $cassandra->execute($batch);
-    }
-
-    /**
-     * @param array $tickerData
-     * @throws ORMInvalidArgumentException
-     * @throws ORMException
-     * @return Asset
-     */
-    public function findOrCreateAsset(array $tickerData) :Asset
-    {
-        if (!$asset = $this->entityManager->getRepository(Asset::class)->findOneBy(['ticker' => $tickerData['asset']])) {
-            $asset = new Asset();
-        }
-        $asset->setPrice($tickerData['price']);
-        $asset->setTicker($tickerData['asset']);
-        $this->entityManager->persist($asset);
-        $this->entityManager->flush();
-
-        return $asset;
-    }
-
-    /**
-     * @param array $tickerData
-     * @throws ORMInvalidArgumentException
-     * @throws ORMException
-     * @return Point
-     */
-    public function createNewPoint(array $tickerData) :Point
-    {
-        $asset = $this->findOrCreateAsset($tickerData);
-        $point = new Point($asset, new \DateTime(), $tickerData['price']);
-
-        return $point;
     }
 
     /**
--- src/Kami/StockBundle/Watcher/StockWatcherInterface.php
+++ src/Kami/StockBundle/Watcher/StockWatcherInterface.php
@@ -3,13 +3,16 @@
 
 namespace Kami\StockBundle\Watcher;
 
+use Kami\StockBundle\Watcher\Binance\BinanceWatcher;
+use Kami\StockBundle\Watcher\Bitfinex\BitfinexWatcher;
+
 interface StockWatcherInterface
 {
+    public function __construct(BinanceWatcher $binanceWatcher, BitfinexWatcher $bitfinexWatcher);
+
     /**
      * Returns graph points to store
-     *
-     * @return array<Point>
      */
-    public function tick() : array;
+    public function tick();
 
 }
