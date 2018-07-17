4bf27840761940c4ffd61fa3a92ebfb84e5a7bb3
rubilnik89 <rubilnik89@gmail.com>
2018-07-17 16:50:49 +0300

    tests

--- tests/KamiStockBundle/Watcher/Bitfinex/BitfinexVolumeWatcherTest.php
+++ tests/KamiStockBundle/Watcher/Bitfinex/BitfinexVolumeWatcherTest.php
@@ -8,6 +8,7 @@ use Kami\AssetBundle\Entity\Asset;
 use Kami\AssetBundle\Repository\AssetRepository;
 use Kami\StockBundle\Watcher\Bitfinex\BitfinexVolumeWatcher;
 use Kami\StockBundle\Watcher\Bittrex\Utils\BittrexClient;
+use Predis\Client;
 use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Tests\Functional\WebTestCase;
 
@@ -24,7 +25,8 @@ class BitfinexVolumeWatcherTest extends WebTestCase
         $em->expects($this->any())->method('getRepository')->willReturn($assetRepoMock);
         $logger = $this->createMock(LoggerInterface::class);
         $client = $this->createMock(BittrexClient::class);
-        $watcher = new BitfinexVolumeWatcher($em, $logger, $client, false);
+        $redis = $this->createMock(Client::class);
+        $watcher = new BitfinexVolumeWatcher($em, $logger, $client, $redis, false);
         $this->assertNull($watcher->updateVolumes());
     }
 }
