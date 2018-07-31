f16f364f0901f745b589ca7e90bf8edfd2bb3e28
Ruslan Komlikov <komlikov.r@gmail.com>
2018-07-31 10:28:18 +0300

    add ccxt lib

--- src/Kami/StockBundle/Watcher/StockWatcherInterface.php
+++ src/Kami/StockBundle/Watcher/StockWatcherInterface.php
@@ -3,10 +3,10 @@
 
 namespace Kami\StockBundle\Watcher;
 
+use Kami\StockBundle\Watcher\Binance\BinanceWatcher;
 use Kami\StockBundle\Watcher\Bitfinex\BitfinexWatcher;
-use Kami\StockBundle\Watcher\CCXT\Binance\BinanceAssetsWatcher;
-use Kami\StockBundle\Watcher\CCXT\Bittrex\BittrexAssetsWatcher;
-use Kami\StockBundle\Watcher\CCXT\CoinMarketCap\CoinMarketCapAssetsWatcher;
+use Kami\StockBundle\Watcher\Bittrex\BittrexWatcher;
+use Kami\StockBundle\Watcher\CoinMarketCap\CoinMarketCapWatcher;
 use Kami\StockBundle\Watcher\Poloniex\PoloniexWatcher;
 
 interface StockWatcherInterface
