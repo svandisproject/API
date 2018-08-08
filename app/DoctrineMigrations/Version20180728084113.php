6c35c61b39976f60ba6aaffe9e859b39b92cc0e6
Iakov Mishchenko <iakov@kamilabs.com>
2018-08-08 14:45:33 +0300

    Fixed migration

--- app/DoctrineMigrations/Version20180728084113.php
+++ app/DoctrineMigrations/Version20180728084113.php
@@ -15,7 +15,8 @@ final class Version20180728084113 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
 
-        $this->addSql('ALTER TABLE ico ALTER token_price TYPE NUMERIC(25, 15) USING token_price::numeric(25, 15)');
+        $this->addSql('ALTER TABLE ico DROP column token_price');
+        $this->addSql('ALTER TABLE table_name ADD COLUMN token_price NUMERIC(25, 15);');
         $this->addSql('ALTER TABLE ico ALTER token_price DROP DEFAULT');
     }
 
