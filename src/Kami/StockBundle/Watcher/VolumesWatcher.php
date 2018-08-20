367aa241222656e602b7cfbbf6ec7b260738db63
Ruslan Komlikov <komlikov.r@gmail.com>
2018-08-20 17:53:34 +0300

    add logger for check work

--- src/Kami/StockBundle/Watcher/VolumesWatcher.php
+++ src/Kami/StockBundle/Watcher/VolumesWatcher.php
@@ -139,6 +139,7 @@ class VolumesWatcher
             $ticker = $asset->getTicker();
             if($data = $this->redis->get($ticker))
             {
+                $this->logger->info("Get ticker - ".$ticker);
                 $data = (array) json_decode($data);
                 $soldAsset = 0;
 
@@ -152,13 +153,16 @@ class VolumesWatcher
                         $soldAsset += $volume / $result[0]['price']->value();
                     }
                 }
-
+                $this->logger->info("Sold asset - ".$soldAsset . " ticker = " . $ticker);
                 if($soldAsset != 0){
+
                     $avgPrice = array_sum($data) / $soldAsset;
                     $asset->setPrice($avgPrice);
                     $asset->setChange($this->changesHelper->setChanges($asset, 'day'));
                     $asset->setWeeklyChange($this->changesHelper->setChanges($asset, 'week'));
                     $asset->setYearToDayChange($this->changesHelper->setChanges($asset, 'year'));
+                    $this->logger->info("Average price = ".$avgPrice . " ticker " .$ticker . " change = " . $asset->getChange());
+                    $this->logger->info("Weeckly change - ".$asset->getWeeklyChange() . " asset year change " . $asset->getYearToDayChange());
                     $this->em->persist($asset);
                     $this->push($asset, $avgPrice, array_sum($data));
                     $this->storeAvgPrice($ticker, $avgPrice, array_sum($data));
