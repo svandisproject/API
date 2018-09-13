5d87c83ab77daf2b783bac124fa0ac54af51a018
rubilnik89 <rubilnik89@gmail.com>
2018-09-13 16:16:00 +0300

    create ctrategy logger

---
+++ src/Kami/WorkerBundle/RequestProcessor/PersistLoggerStep.php
@@ -0,0 +1,64 @@
+<?php
+
+
+namespace Kami\WorkerBundle\RequestProcessor;
+
+use Cassandra\BatchStatement;
+use Kami\Component\RequestProcessor\Artifact;
+use Kami\Component\RequestProcessor\ArtifactCollection;
+use Kami\Component\RequestProcessor\Step\AbstractStep;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+use M6Web\Bundle\CassandraBundle\Cassandra\Client as CassandraClient;
+
+class PersistLoggerStep extends AbstractStep
+{
+    /**
+     * @var CassandraClient
+     */
+    protected $cassandra;
+
+    public function __construct(CassandraClient $cassandra)
+    {
+        $this->cassandra = $cassandra;
+    }
+
+    public function execute(Request $request) : ArtifactCollection
+    {
+        $entity = $this->getArtifact('entity');
+
+        if(true !== $this->getArtifact('validation')) {
+            return new ArtifactCollection();
+        }
+
+        try {
+            $prepared = $this->cassandra->prepare(
+                'INSERT INTO svandis_url_cache.logs (task_type, user_id, time, log) 
+                        VALUES (?, ?, toUnixTimestamp(now()), ?);'
+            );
+            $batch = new BatchStatement(\Cassandra::BATCH_LOGGED);
+
+            $batch->add($prepared, [
+                'task_type' => $entity->getTaskType(),
+                'user_id' =>  new \Cassandra\Decimal($entity->getUserId()),
+                'log' =>  $entity->getLog(),
+            ]);
+
+            $this->cassandra->execute($batch);
+        } catch (\Cassandra\Exception $exception) {
+            throw new BadRequestHttpException('Your request can not be stored', $exception);
+        }
+
+        return new ArtifactCollection([
+            new Artifact('response_data', $entity),
+            new Artifact('status', 200)
+        ]);
+    }
+
+    public function getRequiredArtifacts() : array
+    {
+        return ['validation', 'entity', 'access_granted'];
+    }
+
+
+}
