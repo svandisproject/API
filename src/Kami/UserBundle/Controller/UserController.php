4ba5a728f0e62b1274892c7c74456e5a6dd78a27
Iakov Mishchenko <iakov@kamilabs.com>
2018-11-07 17:25:30 +0200

    Update UserController.php

--- src/Kami/UserBundle/Controller/UserController.php
+++ src/Kami/UserBundle/Controller/UserController.php
@@ -7,7 +7,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use function var_dump;
 
 class UserController extends Controller
 {
