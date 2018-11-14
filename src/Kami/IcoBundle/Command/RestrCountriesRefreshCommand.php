860b9fcf033464dca7b84beac8668a55bb6d5e6b
Roman Zherebko <rubilnik89@gmail.com>
2018-11-14 16:15:50 +0200

    refactor restricted countries command

---
+++ src/Kami/IcoBundle/Command/RestrCountriesRefreshCommand.php
@@ -0,0 +1,53 @@
+<?php
+
+
+namespace Kami\IcoBundle\Command;
+
+
+use Doctrine\ORM\EntityManager;
+use Kami\IcoBundle\Entity\Ico;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class RestrCountriesRefreshCommand extends Command
+{
+    /**
+     * @var EntityManager
+     */
+    protected $manager;
+
+    public function __construct(EntityManager $manager)
+    {
+        $this->manager = $manager;
+
+        parent::__construct();
+    }
+
+
+    protected function configure()
+    {
+        $this->setName('svandis:refresh_restr_countries:sync')
+            ->setDescription('Refresh restricted countries');
+    }
+
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $icos = $this->manager->getRepository(Ico::class)->findAll();
+        foreach ($icos as $ico){
+            $restrCountries = $ico->getRestrictedCountries();
+            $correctData = [];
+            foreach ($restrCountries as $restrCountry){
+                array_push($correctData, $restrCountry);
+            }
+            $ico->setRestrictedCountries([]);
+            $this->manager->persist($ico);
+            $this->manager->flush();
+            $ico->setRestrictedCountries($correctData);
+            $this->manager->persist($ico);
+        }
+        $this->manager->flush();
+    }
+
+}
