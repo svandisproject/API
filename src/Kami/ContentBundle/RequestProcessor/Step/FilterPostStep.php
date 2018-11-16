c4d93390d71bfc5fb4f4c71b2c266cccaf09cf7b
Roman Zherebko <rubilnik89@gmail.com>
2018-11-16 11:05:48 +0200

    reset query builder post filter step

--- src/Kami/ContentBundle/RequestProcessor/Step/FilterPostStep.php
+++ src/Kami/ContentBundle/RequestProcessor/Step/FilterPostStep.php
@@ -2,29 +2,16 @@
 
 namespace Kami\ContentBundle\RequestProcessor\Step;
 
-use Doctrine\Common\Inflector\Inflector;
-use Doctrine\ORM\Mapping\Annotation;
-use Doctrine\ORM\Mapping\ManyToMany;
-use Doctrine\ORM\Mapping\ManyToOne;
-use Doctrine\ORM\Mapping\OneToMany;
-use Doctrine\ORM\Mapping\OneToOne;
 use Doctrine\ORM\QueryBuilder;
 use Kami\ApiCoreBundle\RequestProcessor\Step\Filter\FilterStep;
-use Kami\ApiCoreBundle\Security\AccessManager;
-use Kami\Component\RequestProcessor\ArtifactCollection;
-use Kami\Component\RequestProcessor\ProcessingException;
-use Kami\Component\RequestProcessor\Step\AbstractStep;
-use ReflectionException;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
-use Doctrine\Common\Annotations\Reader;
-use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 
 class FilterPostStep extends FilterStep
 {
     public function applyNeFilter($filter, QueryBuilder $queryBuilder, $shortcut, $index)
     {
         $subQueryBuilder = clone $queryBuilder;
+        $subQueryBuilder->resetDQLParts(['where']);
+        $subQueryBuilder->setParameters([]);
         $subQuery = $subQueryBuilder
             ->where(sprintf($shortcut.'.%s = :%s_value', $filter['property'], $filter['property'].$index))
             ->setParameter(sprintf('%s_value', $filter['property'].$index), $filter['value'])
