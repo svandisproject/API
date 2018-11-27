3a6304e6386e562b87d8c9fbcf0b06f01437d04e
Ruslan Komlikov <komlikov.r@gmail.com>
2018-11-27 19:02:45 +0200

    migrations diff

---
+++ app/DoctrineMigrations/Version20181127170107.php
@@ -0,0 +1,32 @@
+<?php declare(strict_types=1);
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20181127170107 extends AbstractMigration
+{
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('ALTER TABLE "user" ADD onboarded SMALLINT NOT NULL');
+        $this->addSql('ALTER TABLE "user" ADD eth_addresses TEXT DEFAULT NULL');
+        $this->addSql('COMMENT ON COLUMN "user".eth_addresses IS \'(DC2Type:array)\'');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('ALTER TABLE "user" DROP onboarded');
+        $this->addSql('ALTER TABLE "user" DROP eth_addresses');
+    }
+}
